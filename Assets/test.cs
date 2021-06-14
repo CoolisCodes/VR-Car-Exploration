baf7ea02e59416f677443ce89086925aa9bd43be
Kyriakos <kiriakostriadafillidis@gmail.com>
2021-06-14 20:31:28 +0300

    Update test.cs

--- Assets/test.cs
+++ Assets/test.cs
@@ -7,8 +7,12 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    
+    private void Awake()
+    {
+
+    }
 }
+
