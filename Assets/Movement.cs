1b8e51f388cc8539dc20fbb15ea92fe10c09da64
Kyriakos <kiriakostriadafillidis@gmail.com>
2021-06-15 19:06:36 +0300

    Movement developed
    (fix flying problem)

---
+++ Assets/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 100;
+    public Transform mainCamera;
+    public float playerHeight = 1.70f;
+
+    private void Awake()
+    {
+        mainCamera = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        transform.position += mainCamera.rotation * Vector3.forward * speed;
+        transform.position = new Vector3(transform.position.x, playerHeight, transform.position.z);
+    }
+}
